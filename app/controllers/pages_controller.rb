47f9137f131b7e992991971c10654fea010c08ed
stanislas legrez <stanislas27@gmail.com>
2021-03-10 16:11:32 +0100

    sign_out qui marche, btn se déconnecter quand patient_logged_in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_patient!, only: :home
+  # skip_before_action :authenticate_patient!, only: :home
 
   def home    
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :patients
+  devise_scope :patient do
+    get '/patients/sign_out' => 'devise/sessions#destroy'
+  end
   root to: "pages#home"
 end
