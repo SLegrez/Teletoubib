ce7dbb73e1fc4429992c8c7fa2a738d562a69f72
stanislas legrez <stanislas27@gmail.com>
2021-03-11 14:50:34 +0100

    ajouté Model Medecin, controller medecin avec devise, routes, i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < ActionController::Base
   before_action do
     I18n.locale = :fr
   end
-  before_action :authenticate_patient!
+  # before_action :authenticate_patient!
+  # before_action :authenticate_medecin!
+  before_action :authenticate!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def authenticate!
+    if current_patient
+      :authenticate_patient!
+    elsif current_medecin
+      :authenticate_medecin!
+    end
+  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
   devise_for :patients
   devise_scope :patient do
     get '/patients/sign_out' => 'devise/sessions#destroy'
   end
-  root to: "pages#home"
+  devise_for :medecins, path: 'medecins', controllers: { sessions: "medecins/sessions"}
+  devise_scope :medecin do
+    get '/medecins/sign_out' => 'devise/sessions#destroy'
+  end
 end
