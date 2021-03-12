f6ce61178c1135f748338f3c69638d672dbdb900
stanislas legrez <stanislas27@gmail.com>
2021-03-12 15:41:39 +0100

    ajouté Model Créneau

--- app/models/medecin.rb
+++ app/models/medecin.rb
@@ -1,4 +1,5 @@
 class Medecin < ApplicationRecord
+  has_many :creneaux
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_142133) do
+ActiveRecord::Schema.define(version: 2021_03_12_143316) do
+
+  create_table "creneaux", force: :cascade do |t|
+    t.boolean "disponible"
+    t.boolean "indisponible"
+    t.integer "medecin_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "medecins", force: :cascade do |t|
     t.string "email", default: "", null: false
