38f3695452dac7f0ccfa640ca544b74a52336373
stanislas legrez <stanislas27@gmail.com>
2021-03-12 09:51:14 +0100

    supprimé medecin controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_scope :patient do
     get '/patients/sign_out' => 'devise/sessions#destroy'
   end
-  devise_for :medecins, path: 'medecins', controllers: { sessions: "medecins/sessions", registrations: 'medecins/registrations'}
+  devise_for :medecins
   devise_scope :medecin do
     get '/medecins/sign_out' => 'devise/sessions#destroy'
   end
